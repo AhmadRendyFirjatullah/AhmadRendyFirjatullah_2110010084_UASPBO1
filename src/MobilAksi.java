616b9fb492704a4941720f572eff751c43b0b8c6
Ahmad Rendy Firjatullah <a.rendyfirjatullah@gmail.com>
2023-07-07 16:27:59 +0800

    Instance, buat atribut dan panggil method

--- src/MobilAksi.java
+++ src/MobilAksi.java
@@ -1,5 +1,16 @@
 public class MobilAksi {
     public static void main(String[] args) {
-        
+        Mobil M = new Mobil();
+
+        M.namamobil      = "AGYA";
+        M.warna          = "Silver";
+        M.tahunpembuatan = 2018;
+        M.kondisi        = "Baru";
+        M.harga          = 20000000;
+        System.out.println("Nama Mobil      : " + M.namamobil);
+        System.out.println("Warna           : " + M.warna);
+        System.out.println("Tahun Pembuatan : " + M.tahunpembuatan);
+        System.out.println("Kondisi         : " + M.kondisi);
+        System.out.println("Harga           : Rp " + M.harga);
     }
 }
