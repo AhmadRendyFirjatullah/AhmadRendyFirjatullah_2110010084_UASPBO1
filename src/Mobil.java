56f1879141c0fe679035520e03dd098531a23faa
Ahmad Rendy Firjatullah <a.rendyfirjatullah@gmail.com>
2023-07-07 16:29:41 +0800

    Buat Getter and Setter

--- src/Mobil.java
+++ src/Mobil.java
@@ -12,4 +12,36 @@ public class Mobil {
         System.out.println("Selamat Anda Mendapatkan Diskon");
     }
 
+    //Getter and Setter
+    public String getNamamobil() {
+        return namamobil;
+    }
+    public void setNamamobil(String namamobil) {
+        this.namamobil = namamobil;
+    }
+    public String getWarna() {
+        return warna;
+    }
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    public int getTahunpembuatan() {
+        return tahunpembuatan;
+    }
+    public void setTahunpembuatan(int tahunpembuatan) {
+        this.tahunpembuatan = tahunpembuatan;
+    }
+    public String getKondisi() {
+        return kondisi;
+    }
+    public void setKondisi(String kondisi) {
+        this.kondisi = kondisi;
+    }
+    public int getHarga() {
+        return harga;
+    }
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
 }
