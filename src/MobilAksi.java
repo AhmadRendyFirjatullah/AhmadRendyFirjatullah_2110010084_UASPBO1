0e2c89f31798569789af61b7e28ffcb1675192b7
Ahmad Rendy Firjatullah <a.rendyfirjatullah@gmail.com>
2023-07-07 16:31:46 +0800

    Menggunakan method get

--- src/MobilAksi.java
+++ src/MobilAksi.java
@@ -12,5 +12,25 @@ public class MobilAksi {
         System.out.println("Tahun Pembuatan : " + M.tahunpembuatan);
         System.out.println("Kondisi         : " + M.kondisi);
         System.out.println("Harga           : Rp " + M.harga);
+
+        //Hasil dari Getter and Setter
+        Mobil S = new Mobil();
+
+        S.namamobil      = "AVANZA";
+        S.warna          = "Black";
+        S.tahunpembuatan = 2015;
+        S.kondisi        = "Baru";
+        S.harga          = 15000000;
+        System.out.print("Nama Mobil        \t: ");
+        System.out.println(S.getNamamobil());
+        System.out.print("Warnanya          \t: ");
+        System.out.println(S.getWarna());
+        System.out.print("Tahun Pembuatan   \t: ");
+        System.out.println(S.getTahunpembuatan());
+        System.out.print("Kondisi           \t: ");
+        System.out.println(S.getKondisi());
+        System.out.print("Harga             \t: ");
+        System.out.println(S.getHarga());
+        System.out.println("\n");
     }
 }
